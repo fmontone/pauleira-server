383a3bed2962fab059cf6d21688a1bf6a736bafa
Fabio Montone <montone@gmail.com>
2020-02-18 17:24:10 -0300

    Create: Delete Profile Image

--- src/app/models/ProfileImage.js
+++ src/app/models/ProfileImage.js
@@ -1,4 +1,10 @@
 import Sequelize, { Model } from 'sequelize';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import aws from 'aws-sdk';
+
+const s3 = new aws.S3();
 
 class ProfileImage extends Model {
   static init(sequelize) {
@@ -8,6 +14,29 @@ class ProfileImage extends Model {
         url: Sequelize.STRING,
       },
       {
+        hooks: {
+          beforeDestroy: (image, options) => { // eslint-disable-line
+            if (process.env.STORAGE_TYPE === 's3') {
+              return s3
+                .deleteObject({
+                  Bucket: 'pauleiraimages/profile',
+                  Key: image.name,
+                })
+                .promise();
+            }
+            return promisify(fs.unlink)(
+              path.resolve(
+                __dirname,
+                '..',
+                '..',
+                '..',
+                'uploads',
+                'profile',
+                image.name
+              )
+            );
+          },
+        },
         sequelize,
       }
     );
