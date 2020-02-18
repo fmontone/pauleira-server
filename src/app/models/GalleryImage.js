9001af29d64e9fe293c509832317de0ae6666ea0
Fabio Montone <montone@gmail.com>
2020-02-18 18:02:03 -0300

    Create: Delete Gallery Image from AWS

--- src/app/models/GalleryImage.js
+++ src/app/models/GalleryImage.js
@@ -1,4 +1,10 @@
 import Sequelize, { Model } from 'sequelize';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import aws from 'aws-sdk';
+
+const s3 = new aws.S3();
 
 class GalleryImage extends Model {
   static init(sequelize) {
@@ -7,8 +13,37 @@ class GalleryImage extends Model {
         title: Sequelize.STRING,
         description: Sequelize.STRING,
         url: Sequelize.STRING,
+        key: Sequelize.VIRTUAL(),
       },
       {
+        hooks: {
+          beforeDestroy: (image, options) => { // eslint-disable-line
+
+            const imageUrl = image.url.split('/');
+            const imageName = imageUrl[imageUrl.length - 1];
+
+            if (process.env.STORAGE_TYPE === 's3') {
+              return s3
+                .deleteObject({
+                  Bucket: `pauleiraimages/gallery-${image.gallery_id}`,
+                  Key: imageName,
+                })
+                .promise();
+            }
+            return promisify(fs.unlink)(
+              path.resolve(
+                __dirname,
+                '..',
+                '..',
+                '..',
+                'uploads',
+                'galleries',
+                String(image.gallery_id),
+                imageName
+              )
+            );
+          },
+        },
         sequelize,
       }
     );
